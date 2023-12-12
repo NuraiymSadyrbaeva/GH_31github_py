856e8520431bd6cf68ac06acdeef8013d010558f
Nuraiym Sadyrbaeva <120064238+NuraiymSadyrbaeva@users.noreply.github.com>
2023-12-12 21:20:00 +0000

    Py file is created

---
+++ Main.py
@@ -0,0 +1,14 @@
+# Dieses Skript gibt "Hello, World!" aus
+
+print("Hello, World!")
+# Dieses Skript berechnet die Summe von zwei Zahlen
+
+# Zahlen definieren
+zahl1 = 5
+zahl2 = 10
+
+# Summe berechnen
+ergebnis = zahl1 + zahl2
+
+# Ergebnis ausgeben
+print("Die Summe ist:", ergebnis)
